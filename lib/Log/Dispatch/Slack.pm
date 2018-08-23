67d7420e82c15f3c1be73763beb29c2e64b5209d
Bartlomiej Fulanty <starlight@cpan.org>
2018-08-23 13:05:26 +0200

    implement die_on_error property + test

--- lib/Log/Dispatch/Slack.pm
+++ lib/Log/Dispatch/Slack.pm
@@ -4,67 +4,81 @@ package Log::Dispatch::Slack;
 
 use strict;
 use warnings;
- 
-our $VERSION = '0.0004';
 
+our $VERSION = '0.0005';
+
+use Carp;
 use WebService::Slack::WebApi;
 use Log::Dispatch::Output;
 use Try::Tiny;
 use JSON::XS qw/decode_json/;
- 
+
 use base qw( Log::Dispatch::Output );
 
 use Params::Validate qw(validate SCALAR BOOLEAN);
 Params::Validate::validation_options( allow_extra => 1 );
 
 sub APPEND {0}
- 
+
 sub new {
     my $proto = shift;
     my $class = ref $proto || $proto;
- 
+
     my %p = @_;
- 
+
     my $self = bless {}, $class;
- 
+
     $self->_basic_init(%p);
     $self->_make_handle;
- 
+
     return $self;
 }
 
 sub _basic_init {
     my $self = shift;
- 
+
     $self->SUPER::_basic_init(@_);
- 
+
     my %p = validate(
         @_, {
-            token       => { type => SCALAR },
-            channel     => { type => SCALAR },
-            icon        => { type => SCALAR, optional => 1 },
-            username    => { type => SCALAR, optional => 1 },
+            token           => { type => SCALAR },
+            channel         => { type => SCALAR },
+            icon            => { type => SCALAR, optional => 1 },
+            username        => { type => SCALAR, optional => 1 },
+            die_on_error    => { type => BOOLEAN, optional => 1, default => 1 }
         }
     );
- 
-    $self->{channel}    = $p{channel};
-    $self->{token}      = $p{token};
-    $self->{username}   = $p{username};
-    $self->{icon}       = $p{icon};
+
+    $self->{channel}        = $p{channel};
+    $self->{token}          = $p{token};
+    $self->{username}       = $p{username};
+    $self->{icon}           = $p{icon};
+    $self->{die_on_error}   = $p{die_on_error};
+
+    return;
 }
 
 sub _make_handle {
     my $self = shift;
- 
-    $self->{client} = WebService::Slack::WebApi->new(
-        token  => $self->{token},
-    );
+
+    try {
+        $self->{client} = WebService::Slack::WebApi->new(
+            token  => $self->{token},
+        );
+    } catch {
+        my $msg = "Error initialising Webservice::Slack::WebApi: $_";
+        if ($self->{die_on_error}) {
+            croak $msg;
+        }
+        carp $msg;
+    };
+    return;
 }
 
 sub log_message {
     my $self = shift;
     my %p    = @_;
-    
+
     my %post_params = (
         text    => $p{message},
         channel => $p{channel}  || $self->{channel},
@@ -74,7 +88,7 @@ sub log_message {
     }elsif( $self->{icon} ){
         $post_params{icon_url} = $self->{icon};
     }
-    
+
     if( $p{username} ){
         $post_params{username} = $p{username};
     }elsif( $self->{username} ){
@@ -82,15 +96,22 @@ sub log_message {
     }else{
         $post_params{as_user} = 1;
     }
-    
+
     my $response = $self->{client}->chat->post_message( %post_params );
-    
+
     if( ! $response->{ok} ){
-        die( sprintf( "Failed to send message to channel (%s): %s", $self->{channel}, $response->{error} ) );
+
+        my $msg = sprintf( "Failed to send message to channel (%s): %s", $self->{channel}, $response->{error} );
+        if ($self->{die_on_error}) {
+            croak $msg;
+        }
+        carp $msg;
     }
+
+    return;
 }
- 
- 
+
+
 1;
 
 =head1 NAME
@@ -106,6 +127,7 @@ Send log messages to Slack
   log4perl.appender.hipchat=Log::Dispatch::Slack
   log4perl.appender.hipchat.auth_token=your-auth-token
   log4perl.appender.hipchat.channel=channel-to-talk-to
+  log4perl.appender.hipchat.die_on_error=1
 
 =head1 COPYRIGHT
 
@@ -114,4 +136,8 @@ Copyright 2016, Robin Clarke
 =head1 AUTHOR
 
 Robin Clarke <robin@robinclarke.net>
+
+=head1 CONTRIBUTORS
+
+Bartlomiej Fulanty <starlight@cpan.org>
 
